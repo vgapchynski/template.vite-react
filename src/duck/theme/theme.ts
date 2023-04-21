2f6efa553f7992c8e7ab75847997283a23e30bff
Vladislav Gapchynski <>
2023-04-22 00:01:54 +0200

    fix theme config

--- src/duck/theme/theme.ts
+++ src/duck/theme/theme.ts
@@ -1,6 +1,6 @@
 import * as CH from "@chakra-ui/react";
 
-export const THEME = CH.extendBaseTheme({
+export const THEME = CH.extendTheme({
   config: {
     initialColorMode: "system",
     useSystemColorMode: true,
